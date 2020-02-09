157c51c02f2df1a88e41352724747aebf9834627
saint <saintthida@gmail.com>
2020-02-09 17:21:12 +0630

    add all files

---
+++ app/src/main/java/com/tds/oscrazy/ui/home/AddItemFragment.kt
@@ -0,0 +1,31 @@
+package com.tds.oscrazy.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.tds.oscrazy.R
+
+class AddItemFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_add_item, container, false)
+        /*val textView: TextView = root.findViewById(R.id.text_home)
+        homeViewModel.text.observe(this, Observer {
+            textView.text = it
+        })*/
+        return root
+    }
+}
